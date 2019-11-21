6962164dbe654b39a92ac28fe577dc19bffededd
Arman Masangkay <armanmasangkay@smtp10.dia-three.de>
2019-11-21 16:22:58 +0800

    Added choices to have the option to get the division dynamically and the role definition

--- app.py
+++ app.py
@@ -4,16 +4,39 @@ import urllib.parse
 import re
 import webbrowser
 
-res=requests.get('http://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division=europe&leaderboard=1')
+print ('Available divisions')
+print ('-------------------')
+print ('A - Se_Asia\nB - Europe\nC- China\nD - Americas\n')
+divDict={
+   'a':'se_asia',
+   'b':'europe',
+   'c':'china',
+   'd':'americas' 
+}
+val=input("Enter division:").lower()
+division=divDict[val]
+val2=input("Core? [y/n]:").lower()
+role=0
+
+#check if support role or not
+if (val2=='y'):
+    role=1
+else:
+    role=2
+
+#gets the leaderboard data
+res=requests.get('http://www.dota2.com/webapi/ILeaderboard/GetDivisionLeaderboard/v0001?division=' +division+'&leaderboard='+str(role))
 
 data=res.json()
 
 best_player_se_asia=data['leaderboard'][0]['name']
 
-query_string = urllib.parse.urlencode({"search_query" : best_player_se_asia +' full game'})
-#html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 
-#search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-website="http://www.youtube.com/results?" + query_string
-#print(query_string)
+#encode best player data to a url format
+query_string = urllib.parse.urlencode({"search_query" : best_player_se_asia +' full game'})
+
+
+#fetches the video and sorted by upload date
+website="http://www.youtube.com/results?" + query_string +'&sp=CAI%253D'
+
 webbrowser.open_new_tab(website)
